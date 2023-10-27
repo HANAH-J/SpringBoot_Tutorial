8766897fcd04ce82e56ae427b799860313648518
HANAH-J <hana21k@naver.com>
2023-10-27 23:59:37 +0900

    07장 PROJECT 블로그 화면 구성하기

---
+++ src/main/java/site/myduck/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,18 @@
+package site.myduck.springbootdeveloper.dto;
+
+import lombok.Getter;
+import site.myduck.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
